150f1f25776af48b026059e97dbe741058c7ca9d
roberta005 <roberta.devs@hotmail.com>
2022-04-28 23:18:21 -0300

    novos posts

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -10,25 +10,60 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+  state = {
+    posts: [
+      { 
+        nomeUsuario:'paulinha',
+        fotoUsuario: 'https://picsum.photos/50/50',
+        fotoPost: 'https://picsum.photos/200/150'
+      },
+      {
+        nomeUsuario:'Roberta',
+        fotoUsuario:'https://picsum.photos/50/51',
+        fotoPost:'https://picsum.photos/200/151'
+      },
+      {
+        nomeUsuario:'Eder',
+        fotoUsuario:'https://picsum.photos/50/52',
+        fotoPost:'https://picsum.photos/200/152'
+      }
+  ],
+  valorInputNovoPost: "",
+}
+
+  adicionaPost = () => {
+  
+  const novoPost = this.state.valorInputNovoPost;
+
+  const novaPubli = [...this.state.posts, novoPost]
+ 
+  this.setState({ posts: novaPubli });
+  };
+  onChangeInputPost = (event) => {
+    
+    this.setState({ valorInputNovoPost: event.target.value });
+  };
+
+
   render() {
+    const listaDeComponentes = this.state.posts.map((publi)=>{
+      return <li>{publi.nomeUsuario}</li>;
+    })
     return (
-      <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-        nomeUsuario={'Roberta'}
-        fotoUsuario={'https://picsum.photos/id/1011/367/267'}
-        fotoPost={'https://picsum.photos/seed/picsum/536/354'}
-        />
-        <Post
-        nomeUsuario={'Eder'}
-        fotoUsuario={'https://picsum.photos/id/1005/367/267'}
-        fotoPost={'https://picsum.photos/id/645/400/250'}
-        />
-      </MainContainer>
+      <><MainContainer>
+        <Post>
+          <div>{listaDeComponentes}</div>
+        </Post>
+       
+        <input
+          value={this.state.valorInputNovoPost}
+          
+          onChange={this.onChangeInputPost}
+          placeholder={"Nome usuário"} />
+        <button onClick={this.adicionaPost}>Adicionar</button>
+      
+        
+      </MainContainer></>
     );
   }
 }
