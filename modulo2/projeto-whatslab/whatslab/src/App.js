60eb19343fec043e754c6142a20a07f0eae92783
roberta005 <roberta.devs@hotmail.com>
2022-05-01 22:03:34 -0300

    nova versao

--- modulo2/projeto-whatslab/whatslab/src/App.js
+++ modulo2/projeto-whatslab/whatslab/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+
+
+
+
+export class App extends React.Component {
+  state = {
+    campo: [{
+      msg: '',
+      nome: ''
+
+    }
+    ],
+    valorInputNome: '',
+    valorInputMsg: '',
+    
+  }
+  enviarMensagem = () =>{
+    const novoObjeto = {
+      nome: this.state.valorInputNome,
+      msg: this.state.valorInputMsg
+    }
+    const novoArray = [...this.state.campo, novoObjeto]
+    this.setState({campo:novoArray})
+  }
+
+  adicionarUsuário= (event) => {
+    
+      this.setState({valorInputNome: event.target.value})
+    }
+  adicionarMensagem= (event) => {
+    
+      this.setState({valorInputMsg: event.target.value})
+    }
+  
+
+  
+  render(){
+    const lista = this.state.campo.map((item)=>{
+      return (
+        <div><p className='nom'>{item.nome}</p><p>{item.msg}</p></div>
+      )
+    })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    <div className="Container">
+          <div className="box">
+                <div>{lista}</div>
+                <div className="control">
+              <input className= 'inpe' value={this.state.nome} onChange= {this.adicionarUsuário}placeholder="Usuário" />
+              <input className='inp'value={this.state.msg} onChange = {this.adicionarMensagem} placeholder="Mensagem" />
+              <button className = 'botao'onClick={this.enviarMensagem}>Enviar</button>
+                </div>
+          </div>
+        
+      </div>
+    
+  )
+  }
 }
 
 export default App;
