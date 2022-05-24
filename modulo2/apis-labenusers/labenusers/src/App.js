d0a52b20c4095776362ba69a3d08c8dd509da3c7
roberta005 <roberta.devs@hotmail.com>
2022-05-24 19:40:57 -0300

    evolucao labenusers

--- modulo2/apis-labenusers/labenusers/src/App.js
+++ modulo2/apis-labenusers/labenusers/src/App.js
@@ -1,13 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-import Apis from './Components/Api'
+import React from "react"
+import TelaCadastro from "./Components/TelaCadastro"
+import TelaLista from "./Components/TelaLista"
 
-function App() {
-  return (
-    <div className="App">
-      <Apis></Apis>
-    </div>
-  );
+class App extends React.Component{
+
+  state = {
+    telaAtual: "cadastro"
+  }
+
+  trocarTela = () => {
+    this.setState({telaAtual: this.state.telaAtual === "cadastro"
+    ? "lista"
+    : "cadastro"})
+  }
+
+  render(){
+    return(
+      <div>
+        {/* //comparar qual valor atual do state para exibir tela */}
+        {
+          this.state.telaAtual === "cadastro" 
+          ? <TelaCadastro  Tela= {this.trocarTela}/> 
+          : <TelaLista Tela= {this.trocarTela}/>
+        }
+
+
+      </div>
+    )
+  }
+  
 }
 
 export default App;
+
+
+
+
+// function App() {
+//   return (
+//     <div>
+//       <Apis></Apis>
+//     </div>
+//   );
+// }
+
+// export default App;
