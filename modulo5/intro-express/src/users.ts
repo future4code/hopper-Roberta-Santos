3ab22f68c2c7ce7f89c21e2fb56af484d0f6bcfb
roberta005 <roberta.devs@hotmail.com>
2022-08-16 20:46:07 -0300

    intro-express

---
+++ modulo5/intro-express/src/users.ts
@@ -0,0 +1,33 @@
+import {users} from "./index"
+
+export const userList: Array<users> =
+    [ {
+            id: "1",
+            name: "Ana Carolina" ,
+            phone: 333333333,
+            email: "carol@gmail.com",
+            website: "carol.com"},
+
+        {
+            id: "2",
+            name: "João da Silva" ,
+            phone: 666666666,
+            email: "js@gmail.com",
+            website: "js.com"},
+
+        {
+            id: "3",
+            name: "Maria josé" ,
+            phone: 5555555555,
+            email: "mj@gmail.com",
+            website: "mj.com"},
+        {
+            id: "5",
+            name: "Perola da Concha" ,
+            phone: 77777777,
+            email: "pc@gmail.com",
+            website: "pc.com"},
+
+    
+    ]
+
