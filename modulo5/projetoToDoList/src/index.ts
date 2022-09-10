949167004d2cd194e1ffd0e457bd5d1e2519a1c7
roberta005 <roberta.devs@hotmail.com>
2022-09-10 20:37:53 -0300

    alterar users

--- modulo5/projetoToDoList/src/index.ts
+++ modulo5/projetoToDoList/src/index.ts
@@ -20,7 +20,7 @@ const connection = knex({
     database: process.env.DB_NAME,
   },
 });
-
+// Endpoint de criar usuário
 app.post("/user", async (req: Request, res: Response) => {
   let errorCode = 400
   try {
@@ -39,16 +39,104 @@ app.post("/user", async (req: Request, res: Response) => {
 
     await connection.raw(`
       INSERT INTO TodoListUser (id, name, nickname, email)
-      VALUES(${novoUser.id}, "${novoUser.name}", "${novoUser.nickname}", "${novoUser.email}")
+      VALUES(${novoUser.id}, "${novoUser.name}", "${novoUser.nickname}", "${novoUser.email}");
     `)
 
     res.status(200).send("Usuário Cadastrado")
   } catch (error:any) {
     res.status(errorCode).send(error.message)
   }
+});
+
+// Pegar usuário pelo ID
+
+app.get("/user/:id", async (req: Request, res: Response) => {
+  let errorCode = 400
+  try {
+    const user = req.params.id
+
+    if (!user) {
+      throw new Error("Informe o id do usuário")
+    } else {
+      const resultado = await connection.raw(`
+      SELECT * FROM TodoListUser
+      WHERE id = "${user}";
+    `)
+
+    res.status(200).send(resultado[0])
+  }
+    }catch (error:any) {
+    res.status(errorCode).send(error.message)
+  }
+});
+
+
+//Pegar todos os usuários
+
+app.get("/user/all", async (req: Request, res: Response) => {
+  let errorCode = 400
+  try {
+    
+      const resultado = await connection.raw(`
+      SELECT * FROM TodoListUser;
+    `)
+
+    res.status(200).send(resultado[0])
+  }catch (error:any) {
+    res.status(errorCode).send(error.message)
+  }
+});
+
+
+// Editar Usuários
+
+app.put("/user/edit/:id", async (req: Request, res: Response) => {
+  let errorCode = 400
+  try {
+    const id = Number(req.params.id)
+    const name = req.body.name
+    const nickname = req.body.nickname
+    
+    const user = await connection.raw(`
+      SELECT * FROM TodoListUser
+      WHERE id = ${id};
+    `)
+
+    if (user[0].length === 0) {
+      throw new Error("Usuário não encontrado")
+    }
+    if(!id){
+      throw new Error("Informe o ID do usuário que será alterado!");
+      
+    }
+
+    if (!name || !nickname) {
+      throw new Error("É necessário informar a nova alteração")
+    }
+    if(name){
+      await connection.raw(`
+      UPDATE TodoListUser
+      SET name = "${name}"
+      WHERE id = ${id};
+    `)
+    
+    }
+    if(nickname){
+      await connection.raw(`
+      UPDATE TodoListUser
+      SET nickname = "${nickname}"
+      WHERE id = ${id};
+    `)
+    
+    }
+   
+
+    res.status(200).send("Usuário alterado")
+  } catch (error:any) {
+    res.status(errorCode).send(error.message)
+  }
+
 })
-
-
 
 
 
